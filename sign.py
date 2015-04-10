43d42c0aa1a6230ac17e582c7cf349c4ca32520d
LiuJingMing <hk_hoke@163.com>
2015-04-10 13:21:39 +0800

    Update sign.py

--- sign.py
+++ sign.py
@@ -7,18 +7,22 @@ class v2ex:
             "u":self.u,
             "p":self.p,
             "next":"/",
-            "once":re.findall('value="(\d+)" name="once"',s.get("http://www.v2ex.com/signin").text)[0]
+            "once":re.findall('value="(\d+)" name="once"',s.get("https://www.v2ex.com/signin").text)[0]
             }
-        signin=s.post("http://www.v2ex.com/signin",data=payload,headers={'Referer': 'http://www.v2ex.com/signin'})
+        signin=s.post("https://www.v2ex.com/signin",data=payload,headers={'Referer': 'httpss://www.v2ex.com/signin'})
         if signin.text.find("signout")==-1:
             print self.u+" 登录失败！"
         else:
-            try:
-                daily=re.findall('(/mission/daily/redeem\?once=\d+)',s.get("http://www.v2ex.com/mission/daily").text)[0]
-                a=s.get("http://www.v2ex.com"+daily,headers={"Referer":"http://www.v2ex.com/mission/daily"})
-                print self.u+" 签到成功！"
-            except:
-                print self.u+" 签到失败！"
+            print self.u+" 登录成功！"
+            if s.get("https://www.v2ex.com/mission/daily").text.find("fa-ok-sign")!=-1:
+                print self.u+" 已领取过奖励!"
+            else:
+                try:
+                    daily=re.findall('(/mission/daily/redeem\?once=\d+)',s.get("https://www.v2ex.com/mission/daily").text)[0]
+                    a=s.get("https://www.v2ex.com"+daily,headers={"Referer":"https://www.v2ex.com/mission/daily"})
+                    print self.u+" 签到成功！"
+                except:
+                    print self.u+" 签到失败！"
     def __init__(self,u,p):
         self.u=u
         self.p=p
